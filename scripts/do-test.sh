8ec85aa5fc9aa5523bade3302ab818a0e33caab3
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-11 09:49:19 -0800

    Add simple script to run static tests.

---
+++ scripts/do-test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+nfails=0
+for ex in base64 emptyString build amper date ascii encode
+do
+  echo -n "${ex}: "
+  res="ok"
+  if ! python2 examples/examples.py ${ex} >/dev/null
+  then
+    res="FAIL"
+    nfails=$((${nfails} + 1))
+  fi
+  echo $res
+done
+exit ${nfails}
