559b70b46d150f1510deea3ee2e8eba4b27ec7e4
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-11 12:02:13 -0800

    Revert "Run example under strace to figure out what's wrong with it."
    This reverts commit a67696c26c400f51ca36a82bd587a7aa2f12d522.
    
    GC debug "find" invocation.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo find / -name xmlrpc.py -type f
-
 set -e
 
 PYTHON_CMD="${PYTHON_CMD:-"python"}"
@@ -13,11 +11,10 @@ for ex in base64 emptyString build amper date ascii encode
 do
   echo -n "${ex}: "
   res="ok"
-  if ! strace -o strace.out ${PYTHON_CMD} examples/examples.py ${ex} >/dev/null
+  if ! ${PYTHON_CMD} examples/examples.py ${ex} >/dev/null
   then
     res="FAIL"
     nfails=$((${nfails} + 1))
-    cat strace.out >&2
   fi
   echo $res
 done
