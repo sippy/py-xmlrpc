185621ceb22245f392730d9d32ce78c750c014d3
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-12 08:09:08 -0800

    Add bits to make code buildable with python 3.x.

--- src/xmlrpc2to3.h
+++ src/xmlrpc2to3.h
@@ -1,22 +1,22 @@
 /*
  * A python implementation of the xmlrpc spec from www.xmlrpc.com
  *
- * Copyright (C) 2001, Shilad Sen, Sourcelight Technologies, Inc.
- * 
+ * Copyright (C) 2021, Sippy Software, Inc.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * 
+ *
  * The author can be reached at:
  *
  * shilad.sen@sourcelight.com
@@ -25,56 +25,31 @@
  * Sourcelight Technologies, Inc.
  * 906 University Place, Suite B-211
  * Evanston, IL 60201
- * 
+ *
  * This is the main include file for the xmlrpc library.  See the
  * individual header files contain more information about individual
  * objects and procedures.
  *
  */
 
+#ifndef _XMLRPC2TO3_H_
+#define _XMLRPC2TO3_H_
 
-#ifndef _XMLRPC_H_
-#define _XMLRPC_H_
+#define PyString_FromString PyUnicode_FromString
+#define PyString_FromStringAndSize PyUnicode_FromStringAndSize
+#define PyString_AsString PyBytes_AS_STRING
+#define PyString_AS_STRING PyBytes_AS_STRING
+#define PyInt_FromLong PyLong_FromLong
+#define PyInt_Check PyLong_Check
+#define PyInt_CheckExact PyLong_CheckExact
+#define PyInt_AsLong PyLong_AsLong
+#define PyInt_AS_LONG PyLong_AsLong
+#define PyString_GET_SIZE PyBytes_Size
+#define PyString_Concat PyBytes_Concat
+#define PyString_ConcatAndDel PyBytes_ConcatAndDel
+#define _PyString_Resize _PyBytes_Resize
+#define PyObject_Compare(inst, obj) ((inst) == (obj))
+#define Py_FindMethod(x, obj, key) PyObject_GenericGetAttr(obj, PyString_FromString(key))
+#define PyString_Check PyUnicode_Check
 
-
-#define	XMLRPC_VER		"0.8.8.4b"
-#define	XMLRPC_VER_STR		"py-xmlrpc-" XMLRPC_VER
-#define XMLRPC_LIB_STR		"Sourcelight Technologies " XMLRPC_VER_STR
-
-#define	XMLRPC_DATE_FORMAT_US		1
-#define	XMLRPC_DATE_FORMAT_EUROPE	2
-
-#include "rpcBase64.h"
-#include "rpcClient.h"
-#include "rpcDate.h"
-#include "rpcDispatch.h"
-#include "rpcFault.h"
-#include "rpcInclude.h"
-#include "rpcPostpone.h"
-#include "rpcServer.h"
-#include "rpcSource.h"
-#include "rpcUtils.h"
-
-
-extern	PyObject	*rpcError;
-extern	PyObject	*rpcFaultStr;
-extern	int		rpcLogLevel;
-extern	int		rpcDateFormat;
-extern	FILE            *rpcLogger;
-
-
-void		xmlrpcInit(void);
-void		setLogLevel(int level);
-void		setLogger(FILE *logger);
-
-
-#ifdef MSWINDOWS
-	void		rpcNTcleanup(void);
-	int		rpcNTinit(void);
-#endif /* MSWINDOWS */
-
-#if PY_MAJOR_VERSION > 2
-#include "xmlrpc2to3.h"
-#endif
-
-#endif	/* _XMLRPC_H_ */
+#endif /* _XMLRPC2TO3_H_ */
